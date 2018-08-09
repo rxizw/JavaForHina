a46cd96a27d3146306de450bfcb64d8e4789aaa6
rxizw <1440449917@qq.com>
2018-08-09 23:41:38 +0800

    添加通过INI配置删除时间； 通过添加INI配置删除文件夹位置； INI位置在：C:\config.ini

--- Test726/src/www/zhouwei/hina/DeleteVol.java
+++ Test726/src/www/zhouwei/hina/DeleteVol.java
@@ -1,28 +1,46 @@
 package www.zhouwei.hina;
 
 import java.io.File;
+import java.io.IOException;
+
 import org.apache.log4j.Logger;
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Level;
+import org.dtools.ini.*;
 
 public class DeleteVol {
     static Logger log = Logger.getLogger(DeleteVol.class);
     public static void main(String args[]) {
-        File file = new File("E:\\test");
-        File filename[] = file.listFiles();
-        for (int i = 0; i < filename.length; i++) {
-            if (filename[i].isFile()) {
-                long currenttime = System.currentTimeMillis();//获取当前时间；
-                long lastModtime = filename[i].lastModified();//获取最后修改时间；
-                long time = currenttime - lastModtime;
-                if (time / (1000 * 3600 * 24) > 30) {
-                    boolean delete = filename[i].delete();
-                    if (delete) {
-                        System.out.println(filename[i].getName() + "删除成功！");
-                        log.info(filename[i].getName()+"删除成功！");
+        IniFile iniFile = new BasicIniFile();
+        File iniLocal = new File("C:\\config.ini");
+        IniFileReader rad = new IniFileReader(iniFile,iniLocal);
+        try {
+            rad.read();
+            IniSection iniSection = iniFile.getSection("section");
+            IniItem iDay = iniSection.getItem("day");
+            int day1 = Integer.parseInt(iDay.getValue());
+            IniItem iFile = iniSection.getItem("file");
+            String file1 = iFile.getValue();
+            System.out.println(day1);
+            System.out.println(file1);
+            File file = new File(file1);
+            File filename[] = file.listFiles();
+            for (int i = 0; i < filename.length; i++) {
+                if (filename[i].isFile()) {
+                    long currenttime = System.currentTimeMillis();//获取当前时间；
+                    long lastModtime = filename[i].lastModified();//获取最后修改时间；
+                    long time = currenttime - lastModtime;
+                    if (time / (1000 * 3600 * 24) > day1) {
+                        boolean delete = filename[i].delete();
+                        if (delete) {
+                            System.out.println(filename[i].getName() + "删除成功！");
+                            log.info(filename[i].getName()+"删除成功！");
+                        }
                     }
                 }
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
