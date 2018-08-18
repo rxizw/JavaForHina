9b9c0f107ab0a4a99aa3800eb44df2da15e799af
rxizw <1440449917@qq.com>
2018-08-18 14:46:54 +0800

    支持导出为Excel表格，方便查看！

--- CPSpace/src/main/java/com/zhouwei0818/SpaceRead.java
+++ CPSpace/src/main/java/com/zhouwei0818/SpaceRead.java
@@ -1,7 +1,16 @@
 package com.zhouwei0818;
 
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.formula.SheetRange;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+
 import javax.swing.filechooser.FileSystemView;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.text.DecimalFormat;
 
 public class SpaceRead {
@@ -13,14 +22,38 @@ public class SpaceRead {
         return fileSizeString;
     }
 
-    public static void driver(){
+    public static void driver() {
         FileSystemView fsv = FileSystemView.getFileSystemView();
         File[] fs = File.listRoots();
-        for (int i =0;i<fs.length;i++){
-            System.out.println(fsv.getSystemDisplayName(fs[i]));
-            System.out.println("总大小："+FormatFilesSize(fs[i].getTotalSpace()));
-            System.out.println("剩余："+FormatFilesSize(fs[i].getFreeSpace()));
-            System.out.println("已用："+FormatFilesSize(fs[i].getTotalSpace()-fs[i].getFreeSpace()));
+        HSSFWorkbook wb = new HSSFWorkbook();
+        Sheet sheet = wb.createSheet("Sheet1");
+        Row row = sheet.createRow(0);
+        row.createCell(0).setCellValue("盘符");
+        row.createCell(1).setCellValue("总大小");
+        row.createCell(2).setCellValue("剩余");
+        row.createCell(3).setCellValue("可用");
+        for (int i = 0; i < fs.length; i++) {
+                row = sheet.createRow(i+1);
+                row.createCell(0).setCellValue(fsv.getSystemDisplayName(fs[i]));
+                row.createCell(1).setCellValue(FormatFilesSize(fs[i].getTotalSpace()));
+                row.createCell(2).setCellValue(FormatFilesSize(fs[i].getFreeSpace()));
+                row.createCell(3).setCellValue(FormatFilesSize(fs[i].getTotalSpace() - fs[i].getFreeSpace()));
+                System.out.println(fsv.getSystemDisplayName(fs[i]));
+                System.out.println("总大小：" + FormatFilesSize(fs[i].getTotalSpace()));
+                System.out.println("剩余：" + FormatFilesSize(fs[i].getFreeSpace()));
+                System.out.println("已用：" + FormatFilesSize(fs[i].getTotalSpace() - fs[i].getFreeSpace()));
+        }
+        try {
+            FileOutputStream fout = new FileOutputStream("Space.xls");
+            try {
+                wb.write(fout);
+                fout.close();
+                System.out.println("导出成功！");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
     }
     public static void main(String args[]){
